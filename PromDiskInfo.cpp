19b63d9c97a54ef835091b9dac7c2bb6311912f2
liyue <liyue80@gmail.com>
2023-02-10 14:54:18 +0800

    add option and argument -h, -o

--- PromDiskInfo.cpp
+++ PromDiskInfo.cpp
@@ -3,15 +3,80 @@
 
 #include "stdafx.h"
 #include <iostream>
-
+#include <shellapi.h>
 #include "Priscilla/UtilityFx.h"
 #include "DiskInfo.h"
 
-int main()
+void printUsage(BOOL bVerbose = FALSE)
 {
+
+}
+
+constexpr UINT_PTR str2int(const TCHAR* str, int h = 0)
+{
+    return !str[h] ? 5381 : (str2int(str, h + 1) * 33) ^ str[h];
+}
+
+int main(
+#ifndef UNICODE
+int argc, char **argv
+#endif // !UNICODE
+)
+{
+#ifdef UNICODE
+    int argc;
+    LPTSTR cmdLine = ::GetCommandLine();
+    LPTSTR *argv = ::CommandLineToArgvW(cmdLine, &argc);
+#endif // UNICODE
+
+    CString strOutFile;
+    for (int i = 1; i < argc; i++)
+    {
+        CString v = argv[i];
+
+        if (_tcschr(_T("-/"), v.GetAt(0)) != NULL) // begin with '-' or '/'
+        {
+            v = v.TrimLeft(_T("-/"));
+
+            switch (str2int((LPCTSTR)v))
+            {
+            case str2int(_T("?")):
+            case str2int(_T("h")):
+            case str2int(_T("help")):
+                printUsage(TRUE);
+                return 0;
+
+            case str2int(_T("o")):
+            case str2int(_T("out")):
+                if (++i < argc)
+                {
+                    strOutFile = argv[i];
+                }
+                else
+                {
+                    _tprintf_s(_T("Missing argument of %s\n\n"), argv[i]);
+                    printUsage();
+                    return -1;
+                }
+                break;
+
+            default:
+                _tprintf_s(_T("Unknown option %s\n\n"), argv[i]);
+                printUsage();
+                return -1;
+            }
+        }
+        else
+        {
+            _tprintf_s(_T("Invalid argument '%s'\n\n"), (LPCTSTR)v);
+            printUsage();
+            return -1;
+        }
+    }
+
     SetDebugMode(0);
     CDiskInfo diskInfo;
-    diskInfo.SaveText(L"C:\\Temp\\~DiskInfo.txt");
+    diskInfo.SaveText(strOutFile);
 
     return 0;
 }
